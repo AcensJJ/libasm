606a84b5e795ef1e3a198d2e301fd31265a1d0fd
AcensJJ <uentosacens@gmail.com>
2020-01-15 21:22:03 +0100

    add rsi to struct

--- srcs/ft_list_push_front.s
+++ srcs/ft_list_push_front.s
@@ -7,6 +7,8 @@ section .text
 _ft_list_push_front:
 	cmp		rdi, 0
 	je		_exit_error
+	cmp		rsi, 0
+	je		_exit_error
 	push	r12
 	mov		r12, rdi
 	mov		rdi, 16
@@ -17,6 +19,7 @@ _ft_list_push_front:
 _push_front:
 	push	r13
 	mov		r13, [r12]
+	mov		[rax + 0] , rsi
 	mov		[rax + 8] , r13
 	mov		r12, rax
 
